b7cbbd5979a11a49cbdc9126383eb7dc8598d7b4
Mateusz Domagala <mateuszd@haddad.com>
2022-01-18 18:06:59 +0100

    Quick fix.

--- ResponseModel/CompanySearchResultResponse.cs
+++ ResponseModel/CompanySearchResultResponse.cs
@@ -5,6 +5,7 @@ namespace WebAPI.ResponseModel
 {
 	public class CompanySearchResultResponse
 	{
+		public long Id { get; set; }
 		public string Name { get; set; }
 		public int EstablishmentYear { get; set; }
 
